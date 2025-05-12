05a13c4f55cdbb4dd023de63c21f17512a9690e3
Andres Campi <andreskampi@gmail.com>
2025-05-11 19:19:23 -0500

    Primer commit: inventario front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { PrimeNG } from 'primeng/config';
+
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet,ToastModule,ConfirmDialogModule],
+    providers:[MessageService,ConfirmationService],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'campidev';
+  constructor(private primeng: PrimeNG) {}
+  title = 'Campidev';
+  ngOnInit() {
+    this.primeng.ripple.set(true);
+}
+
+menuOpen = false;
+
+toggleMenu() {
+  this.menuOpen = !this.menuOpen;
+}
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,93 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import Material from '@primeng/themes/aura';
 import { routes } from './app.routes';
+import { FilterMatchMode, MessageService } from 'primeng/api';
+import { provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
+    provideHttpClient(),       
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: false,
+      inputVariant: 'filled' ,
+        theme: {
+            preset: Material,
+            options: {
+              prefix: 'p',
+              darkModeSelector: 'false',
+              cssLayer: false,
+              
+          }
+        },
+        zIndex: {
+          modal: 1100,    // dialog, sidebar
+          overlay: 1000,  // dropdown, overlaypanel
+          menu: 1000,     // overlay menus
+          tooltip: 1100   // tooltip
+      },
+      filterMatchModeOptions: {
+        text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
+        numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
+        date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
+    },
+    translation:{
+      accept: 'Aceptar',
+      reject: 'Cancelar',
+      choose: 'Seleccionar',
+      upload: 'Subir Archivos',
+      cancel: 'Cancelar',
+      dayNames: [
+        'domingo',
+        'lunes',
+        'martes',
+        'miércoles',
+        'jueves',
+        'viernes',
+        'sábado',
+      ],
+      dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+      dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+      monthNames: [
+        'enero',
+        'febrero',
+        'marzo',
+        'abril',
+        'mayo',
+        'junio',
+        'julio',
+        'agosto',
+        'septiembre',
+        'octubre',
+        'noviembre',
+        'diciembre',
+      ],
+      monthNamesShort: [
+        'ene',
+        'feb',
+        'mar',
+        'abr',
+        'may',
+        'jun',
+        'jul',
+        'ago',
+        'sep',
+        'oct',
+        'nov',
+        'dic',
+      ],
+      today: 'Hoy',
+      clear: 'Limpiar',
+      weak: 'Débil',
+      medium: 'Medio',
+      strong: 'Fuerte',
+      passwordPrompt: 'Introduce la contraseña',
+    }
+    }),MessageService]
 };
